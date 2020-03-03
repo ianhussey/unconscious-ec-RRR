7e6e273dc83b5af48901ccd3c409bbedcf74b153
ianhussey <ian.hussey@ugent.be>
2020-03-03 11:44:19 +0100

    updated forest plot with ns

--- analysis/forest_plots_for_publication.R
+++ analysis/forest_plots_for_publication.R
@@ -1,6 +1,22 @@
 # Open and run analysis.Rmd so that the results_1, results_2, results_3, and results_4 objects are present in your environment. 
 # Then run this script. 
 
+
+# Ns
+nrow(data_1)
+# >[1] 1340
+
+nrow(data_2)
+# >[1] 1007
+
+nrow(data_3)
+# >[1] 755
+
+nrow(data_4)
+# >[1] 1060
+
+
+
 setwd("~/git/unconscious-ec-RRR/analysis")
 
 pdf(NULL)
@@ -17,8 +33,8 @@ metafor::forest(results_1$fitted_model,
                 at = c(-0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8, 1.0),
                 refline = 0,
                 mlab = add_heterogeneity_metrics_to_forest(results_1$fitted_model))
-text(-1.1, 14, "Olson & Fazio (2001) exclusions", pos = 4)
-text(1.6, 13.85, substitute(paste("Hedges' ", italic('g'), " [95% CI]")), pos = 2)
+text(-1.1, 14, substitute(paste("(a) Olson & Fazio (2001) exclusions (", italic('n'), " = 1340)")), pos = 4)
+text(1.6, 13.97, substitute(paste(italic('g'), " [95% CI]")), pos = 2)
 
 
 metafor::forest(results_2$fitted_model,
@@ -28,8 +44,8 @@ metafor::forest(results_2$fitted_model,
                 at = c(-0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8, 1.0),
                 refline = 0,
                 mlab = add_heterogeneity_metrics_to_forest(results_2$fitted_model))
-text(-1.1, 14, "Olson & Fazio (2001) modified exclusions", pos = 4)
-text(1.6, 13.85, substitute(paste("Hedges' ", italic('g'), " [95% CI]")), pos = 2)
+text(-1.1, 14, substitute(paste("(b) Olson & Fazio (2001) modified exclusions (", italic('n'), " = 1007)")), pos = 4)
+text(1.6, 13.97, substitute(paste(italic('g'), " [95% CI]")), pos = 2)
 
 
 metafor::forest(results_3$fitted_model,
@@ -39,8 +55,8 @@ metafor::forest(results_3$fitted_model,
                 at = c(-0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8, 1.0),
                 refline = 0,
                 mlab = add_heterogeneity_metrics_to_forest(results_3$fitted_model))
-text(-1.1, 14, "Bar-Anan et al. (2010) exclusions", pos = 4)
-text(1.6, 13.85, substitute(paste("Hedges' ", italic('g'), " [95% CI]")), pos = 2)
+text(-1.1, 14, substitute(paste("(c) Bar-Anan et al. (2010) exclusions (", italic('n'), " = 755)")), pos = 4)
+text(1.6, 13.97, substitute(paste(italic('g'), " [95% CI]")), pos = 2)
 
 
 metafor::forest(results_4$fitted_model,
@@ -50,8 +66,8 @@ metafor::forest(results_4$fitted_model,
                 at = c(-0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8, 1.0),
                 refline = 0,
                 mlab = add_heterogeneity_metrics_to_forest(results_4$fitted_model))
-text(-1.1, 14, "Bar-Anan et al. (2010) modified exclusions", pos = 4)
-text(1.6, 13.85, substitute(paste("Hedges' ", italic('g'), " [95% CI]")), pos = 2)
+text(-1.1, 14, substitute(paste("(c) Bar-Anan et al. (2010) modified exclusions (", italic('n'), " = 1060)")), pos = 4)
+text(1.6, 13.97, substitute(paste(italic('g'), " [95% CI]")), pos = 2)
 
 
 p1 <- recordPlot()
